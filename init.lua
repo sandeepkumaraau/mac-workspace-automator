58ffa9ecd7d7c37ae1fea5cf4331d4ca08eb15a6
Sandeep Kumar <127148551+sandeepkumaraau@users.noreply.github.com>
2025-12-30 17:49:35 +0100

    Add job search automation sequence with updated hotkeys and URLs

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ hs.alert.show("Hammerspoon Automation Ready")
 
 
 -- Automation Sequence for coding setup
--- Hotkey: Cmd + Option + Ctrl + C
+-- Hotkey: Cmd + Ctrl + C
 hs.hotkey.bind({"cmd" , "ctrl"}, "C", function()
     
     hs.alert.show("Starting Coding Setup...")
@@ -33,7 +33,7 @@ hs.hotkey.bind({"cmd" , "ctrl"}, "C", function()
         window.fullscreenchrome()
     end)
 
-    hs.timer.doAfter(5, function()
+    hs.timer.doAfter(3, function()
         window.detachCopilot()
     end)
 
@@ -41,7 +41,7 @@ end)
 
 
 -- Automation Sequence for Study setup
--- Hotkey: Cmd + Option + Ctrl + S
+-- Hotkey: Cmd + Ctrl + S
 hs.hotkey.bind({"cmd" , "ctrl"}, "S", function()
 
     hs.alert.show("Starting Study Setup...")
@@ -65,3 +65,39 @@ hs.hotkey.bind({"cmd" , "ctrl"}, "S", function()
 end)
 
 
+
+
+-- Automation Sequence for jobSearch setup
+-- Hotkey: Cmd + Ctrl + J
+hs.hotkey.bind({"cmd" , "ctrl"}, "J", function()
+
+    hs.alert.show("Starting Job Search Setup...")
+    
+    display.connectSidecar()
+    system.enableDND()
+
+    hs.timer.doAfter(1, function()
+        system.launchChrome()
+        hs.urlevent.openURL("https://jobs.infineon.com/careers?domain=infineon.com")
+        hs.urlevent.openURL("https://www.linkedin.com/jobs/")
+        hs.urlevent.openURL("https://www.karriere.at/jobs")
+        hs.urlevent.openURL("https://www.kaerntnerjobs.at/jobs?region=91,28,95,96,93,94,97,98,100,92,99&category=10,11,18,27")
+
+        window.fullscreenchrome()
+        window.openGeminiWindow()
+    end)
+
+    hs.timer.doAfter(2, function()
+        window.moveGeminiToPrimary()
+    end)
+
+    hs.timer.doAfter(2, function()
+        system.launchVScode()
+    end)
+
+    hs.timer.doAfter(3, function()
+        window.detachCopilot()
+    end)
+
+
+end)
